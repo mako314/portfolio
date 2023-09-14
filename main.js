e600346ece989a17b3ef6c57e392dd6c2a265d58
Macolister <bispo.mac@gmail.com>
2023-09-14 18:32:00 -0400

    gray screen, starting over

--- main.js
+++ main.js
@@ -5,58 +5,10 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 
 
-
-
 if ( WebGL.isWebGLAvailable() ) {
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-
-    // camera.position.z = 15;
-
-
-    //Camera positoning and controls
-    camera.position.set(500, 500, 500);
-    const controls = new OrbitControls(camera, renderer.domElement);
-
-    controls.mouseButtons = {
-        LEFT: THREE.MOUSE.ROTATE,
-        MIDDLE: THREE.MOUSE.DOLLY,
-        RIGHT: THREE.MOUSE.PAN
-    }
-
-    controls.update();
-    
-
-    /// lighting ///
-    const light = new THREE.AmbientLight(0xffaaff);
-    light.position.set(10, 10, 10);
-    scene.add(light);
-
-    // Loads the model I imported
-    const loader = new GLTFLoader();
-
-    loader.load( 'public/attempt3.glb', function ( gltf ) {
-
-        scene.add( gltf.scene );
-
-    }, undefined, function ( error ) {
-
-        console.error( error );
-
-    } );
-
-    function animate() {
-        requestAnimationFrame( animate );
-        renderer.render( scene, camera );
-    }
-
-    renderer.render(scene, camera);
-    
+	// Initiate function or other initializations here
+	animate();
 
 } else {
 
