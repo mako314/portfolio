54880828a7edb67bd3a6bfe36d8795968a73f2e4
Macolister <bispo.mac@gmail.com>
2023-09-14 19:07:57 -0400

    model position

--- main.js
+++ main.js
@@ -7,7 +7,48 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 if ( WebGL.isWebGLAvailable() ) {
 
-	// Initiate function or other initializations here
+	const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    //LIGHTING//
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    scene.add( directionalLight );
+
+
+    camera.position.set( 0, 20, 100 );
+
+    // renderer.setClearColor(0xffffff); // Set a white clear color
+
+
+    function animate() {
+        requestAnimationFrame( animate );
+        renderer.render( scene, camera );
+    }
+
+    const loader = new GLTFLoader();
+
+    loader.load( 'public/attempt3.glb', function ( gltf ) {
+
+        const model = gltf.scene
+
+        model.position.setX(0);
+        model.position.setY(0);
+        model.position.setZ(0);
+
+        scene.add( model );
+
+
+
+    }, undefined, function ( error ) {
+
+        console.error( error );
+
+    } );
+
 	animate();
 
 } else {
