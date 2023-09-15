437055c5d893970e33a45c5716d429e45ec55214
Macolister <bispo.mac@gmail.com>
2023-09-15 19:01:03 -0400

    add a plane

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ if ( WebGL.isWebGLAvailable() ) {
     const scene = new THREE.Scene();
 
     const camera = new THREE.PerspectiveCamera(
-        75,
+        45,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
@@ -30,8 +30,10 @@ if ( WebGL.isWebGLAvailable() ) {
     
     // camera.position.z = 5;
     // camera.position.y = 2;
+
     //                 //x y z
-    camera.position.set(0, 2 ,5 );
+    camera.position.set(-10, 30 ,30 );
+
     //Changes position of camera. must come after camera.position.set
     orbit.update
 
@@ -40,6 +42,11 @@ if ( WebGL.isWebGLAvailable() ) {
     const boxMaterial = new THREE.MeshBasicMaterial({color : 0x00FF00})
     const box = new THREE.Mesh(boxGeometry, boxMaterial)
     scene.add(box)
+
+    const planeGeometry = new THREE.PlaneGeometry(30, 30);
+    const planeMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF})
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    scene.add(plane)
 
     box.rotation.x = 5;
     box.rotation.y = 5;
