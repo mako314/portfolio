0856b09aa01513068050a21ab7fac5340eb14c13
Macolister <bispo.mac@gmail.com>
2023-09-18 20:39:01 -0400

    add vertex and fragment shaders

--- main.js
+++ main.js
@@ -238,13 +238,28 @@ if ( WebGL.isWebGLAvailable() ) {
     const lastPointZ = plane2.geometry.attributes.position.array.length - 1;
     // plane2.geometry.attributes.position.array[lastPointZ] -= 10 * Math.random();
     //----------------------------------------------------------------------------------------------
+    
+    
     //-----------------Adding sphere with vertex shader and fragment shaders-----------------------------------------------------------------------
 
     const sphere2Geometry = new THREE.SphereGeometry(4);
 
+    //We're adding the bottom commented out stuff to our index.html as scripts, and not within our main.js
+    // const vShader = `
+    //     void main() {
+    //         gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    // }
+    // `;
+
+    // const fShader = `
+    //     void main() {
+    //         gl_FragColor = vec4(0.5, 0.5, 1.0, 1.0);
+    //     }  
+    // `;
+
     const sphere2Material = new THREE.ShaderMaterial({
-        vertexShader: vShader,
-        fragmentShader: fshader
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
     });
 
     const sphere2 = new THREE.Mesh(sphere2Geometry, sphere2Material);
